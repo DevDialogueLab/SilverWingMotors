0d264f61a0946b84c40dd78efcde015aaffad518
davidgoulart-dev <davidgoulart.tae@gmail.com>
2023-11-25 21:04:55 -0300

    Check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
